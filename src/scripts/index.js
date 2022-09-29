966f5a512a788598cac69a0c9068d5f9a3336079
Saad Malik <saad_malik200@hotmail.com>
2022-09-29 14:57:57 +0200

    ex1

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,33 @@
 import "../styles/main.scss";
-import "babel-polyfill"
+import "babel-polyfill";
 
 const modal = document.getElementById("myModal");
+
 let promiseOfModal = new Promise(function (resolve) {
-    window.setTimeout(function () {
-        resolve(modal)
-    }, (1000 * 60));
+  window.setTimeout(function () {
+    resolve(modal);
+  }, 1000 * 4);
 });
 
-promiseOfModal.then(function(val) {
-    console.log("User has been on the page for 60 seconds");
-    val.style.display = "block";
-})
+const asyncFunc = async () => {
+  await promiseOfModal;
+
+  console.log("User has been on the page for 60 seconds");
+  modal.style.display = "block";
+};
+
+asyncFunc();
+
+// promiseOfModal.then(function (val) {
+//   console.log("User has been on the page for 60 seconds");
+//   val.style.display = "block";
+// });
 
 modal.addEventListener("click", (e) => {
-    switch(e.target.className) {
-        case "close":
-        case "modal":
-        modal.style.display = "none"
-        break;
-    }
-})
-
+  switch (e.target.className) {
+    case "close":
+    case "modal":
+      modal.style.display = "none";
+      break;
+  }
+});
