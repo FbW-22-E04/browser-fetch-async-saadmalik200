375495afbe24a99204746858993c0f147053a9ca
Saad Malik <saad_malik200@hotmail.com>
2022-09-29 23:49:03 +0200

    ex 2 final

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,19 +2,19 @@ import "../styles/main.scss";
 import "babel-polyfill";
 
 const modal = document.getElementById("myModal");
+// const button = document.querySelector("btn:hover");
 
+const asyncFunc = async () => {
+  let result = await promiseOfModal;
+
+  console.log("User has been on the page for 60 seconds");
+  result.style.display = "block";
+};
 let promiseOfModal = new Promise(function (resolve) {
   window.setTimeout(function () {
     resolve(modal);
-  }, 1000 * 4);
+  }, 1000 * 14);
 });
-
-const asyncFunc = async () => {
-  await promiseOfModal;
-
-  console.log("User has been on the page for 60 seconds");
-  modal.style.display = "block";
-};
 
 asyncFunc();
 
@@ -31,3 +31,23 @@ modal.addEventListener("click", (e) => {
       break;
   }
 });
+
+//animation end
+const continueButton = document.getElementById("continue");
+continueButton.addEventListener("mouseover", () => {
+  onAnimationComplete();
+});
+
+async function onAnimationComplete() {
+  const buttonAnimationEnd = await onAnimationStart(continueButton);
+  buttonAnimationEnd.classList.add("btn-info");
+  alert("Continue to subscription!");
+}
+
+function onAnimationStart(elem) {
+  return new Promise((resolve) => {
+    elem.addEventListener("animationend", () => {
+      resolve(elem);
+    });
+  });
+}
